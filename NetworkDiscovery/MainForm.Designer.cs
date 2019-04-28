98e0ef0d7146b35ad1a761cd8efbb80436350e0f
Mortens4444 <mortens.4444@gmail.com>
2019-04-28 11:59:48 +0200

    Initial commit

---
+++ NetworkDiscovery/MainForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace NetworkDiscovery
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.pMain = new System.Windows.Forms.Panel();
+			this.gbMain = new System.Windows.Forms.GroupBox();
+			this.lvDevices = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.pMain.SuspendLayout();
+			this.gbMain.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pMain
+			// 
+			this.pMain.Controls.Add(this.gbMain);
+			this.pMain.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pMain.Location = new System.Drawing.Point(0, 0);
+			this.pMain.Name = "pMain";
+			this.pMain.Size = new System.Drawing.Size(800, 450);
+			this.pMain.TabIndex = 1;
+			// 
+			// gbMain
+			// 
+			this.gbMain.Controls.Add(this.lvDevices);
+			this.gbMain.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.gbMain.Location = new System.Drawing.Point(0, 0);
+			this.gbMain.Name = "gbMain";
+			this.gbMain.Size = new System.Drawing.Size(800, 450);
+			this.gbMain.TabIndex = 0;
+			this.gbMain.TabStop = false;
+			// 
+			// lvDevices
+			// 
+			this.lvDevices.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader9,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+			this.lvDevices.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lvDevices.FullRowSelect = true;
+			this.lvDevices.HideSelection = false;
+			this.lvDevices.Location = new System.Drawing.Point(3, 16);
+			this.lvDevices.Name = "lvDevices";
+			this.lvDevices.Size = new System.Drawing.Size(794, 431);
+			this.lvDevices.TabIndex = 0;
+			this.lvDevices.UseCompatibleStateImageBehavior = false;
+			this.lvDevices.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "IP Address";
+			this.columnHeader1.Width = 91;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Hostname";
+			this.columnHeader2.Width = 119;
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Text = "MAC address";
+			this.columnHeader3.Width = 121;
+			// 
+			// columnHeader4
+			// 
+			this.columnHeader4.Text = "Interface name";
+			this.columnHeader4.Width = 99;
+			// 
+			// columnHeader9
+			// 
+			this.columnHeader9.Text = "Interface type";
+			this.columnHeader9.Width = 89;
+			// 
+			// columnHeader5
+			// 
+			this.columnHeader5.Text = "Interface description";
+			this.columnHeader5.Width = 124;
+			// 
+			// columnHeader6
+			// 
+			this.columnHeader6.Text = "Intereface speed";
+			this.columnHeader6.Width = 96;
+			// 
+			// columnHeader7
+			// 
+			this.columnHeader7.Text = "Download";
+			// 
+			// columnHeader8
+			// 
+			this.columnHeader8.Text = "Upload";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.pMain);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Network Discovery";
+			this.pMain.ResumeLayout(false);
+			this.gbMain.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel pMain;
+		private System.Windows.Forms.GroupBox gbMain;
+		private System.Windows.Forms.ListView lvDevices;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.ColumnHeader columnHeader3;
+		private System.Windows.Forms.ColumnHeader columnHeader4;
+		private System.Windows.Forms.ColumnHeader columnHeader9;
+		private System.Windows.Forms.ColumnHeader columnHeader5;
+		private System.Windows.Forms.ColumnHeader columnHeader6;
+		private System.Windows.Forms.ColumnHeader columnHeader7;
+		private System.Windows.Forms.ColumnHeader columnHeader8;
+	}
+}
+
---
+++ NetworkDiscovery/MainForm.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace NetworkDiscovery
+{
+	public partial class MainForm : Form
+	{
+		private const int NO_ERROR = 0;
+		private const int ERROR_BAD_NET_NAME = 67;
+		private const int ERROR_BUFFER_OVERFLOW = 111;
+		private const int ERROR_GEN_FAILURE = 31;
+		private const int ERROR_INVALID_PARAMETER = 87;
+		private const int ERROR_NOT_SUPPORTED = 50;
+
+		[DllImport("iphlpapi.dll", ExactSpelling = true)]
+		public static extern int SendARP(uint DestIP, uint SrcIP, byte[] pMacAddr, ref int PhyAddrLen);
+
+		public MainForm()
+		{
+			InitializeComponent();
+
+			Discovery();
+		}
+
+		private void Discovery()
+		{
+			var hostname = Dns.GetHostName();
+			var ips = Dns.GetHostEntry(hostname);
+
+			foreach (var ip in ips.AddressList)
+			{
+				if (ip.AddressFamily == AddressFamily.InterNetworkV6)
+				{
+					DiscoverHost(ip.ToString());
+				}
+				else
+				{
+					var subnet = ip.ToString().Substring(0, ip.ToString().LastIndexOf('.'));
+
+					var trds = new Thread[255];
+					for (int i = 1; i < 255; i++)
+					{
+						trds[i] = new Thread(DiscoverHost)
+						{
+							Name = $"{subnet}.{i}"
+						};
+						trds[i].Start(trds[i].Name);
+					}
+				}
+			}
+		}
+
+		private void DiscoverHost(object ipAddressObj)
+		{
+			string ipAddressStr = ipAddressObj.ToString();
+			var items = new ArrayList();
+
+			try
+			{
+				ListViewItem item;
+				item = new ListViewItem(ipAddressStr);
+				item.SubItems.Add(GetHostName(ipAddressStr));
+				var ipAddress = IPAddress.Parse(ipAddressStr);
+				var phyhicalAddress = GetMACFromNetworkComputer(ipAddress);
+				item.SubItems.Add(MacAddressToString(phyhicalAddress));
+
+				if (IsLocalIPAddress(ipAddressStr))
+				{
+					var networkInterface = NetworkInterface.GetAllNetworkInterfaces()
+						.FirstOrDefault(nic => nic.GetIPProperties().UnicastAddresses
+							.FirstOrDefault(ipInfo => ipInfo.Address.ToString() == ipAddressStr) != null);
+
+					if (networkInterface != null)
+					{
+						item.SubItems.Add(networkInterface.Name);
+						item.SubItems.Add(networkInterface.NetworkInterfaceType.ToString());
+						item.SubItems.Add(networkInterface.Description);
+						item.SubItems.Add(FormatValue(networkInterface.Speed, true));
+
+						var stats = networkInterface.GetIPv4Statistics();
+						var previousSentBytes = stats.BytesSent;
+						var previousReceivedBytes = stats.BytesReceived;
+						Thread.Sleep(1000);
+						stats = networkInterface.GetIPv4Statistics();
+
+						item.SubItems.Add(FormatValue(stats.BytesSent, previousSentBytes));
+						item.SubItems.Add(FormatValue(stats.BytesReceived, previousReceivedBytes));
+					}
+					else
+					{
+						for (int i = 0; i < 6; i++)
+						{
+							item.SubItems.Add("N/A");
+						}
+					}
+				}
+				else
+				{
+					for (int i = 0; i < 6; i++)
+					{
+						item.SubItems.Add(String.Empty);
+					}
+				}
+
+				AddItemToList(lvDevices, item);
+				Update();
+			}
+			catch { }
+		}
+
+		private static string FormatValue(long allSentBytes1, long allSentBytes2)
+		{
+			var sentBytesPerSecond = allSentBytes1 - allSentBytes2;
+			return FormatValue(sentBytesPerSecond, false);
+		}
+
+		private static string FormatValue(long sentBytesPerSecond, bool bitPerSec)
+		{
+			var unit = bitPerSec ? "Bit" : "B";
+			if (ChangeValue(ref sentBytesPerSecond, ref unit, bitPerSec ? "KBit" : "KB"))
+			{
+				if (ChangeValue(ref sentBytesPerSecond, ref unit, bitPerSec ? "MBit" : "MB"))
+				{
+					ChangeValue(ref sentBytesPerSecond, ref unit, bitPerSec ? "GBit" : "GB");
+				}
+			}
+			return $"{sentBytesPerSecond} {unit}/s";
+		}
+
+		private static bool ChangeValue(ref long value, ref string unit, string newUnit)
+		{
+			if (value > 1024)
+			{
+				value = value / 1024;
+				unit = newUnit;
+				return true;
+			}
+			return false;
+		}
+
+		delegate void voidResultListViewListViewItemParams(ListView listView, ListViewItem listViewItem);
+		private void AddItemToList(ListView listView, ListViewItem listViewItem)
+		{
+			try
+			{
+				if (!listView.InvokeRequired)
+				{
+					var backColor = listView.Items.Count % 2 == 0 ? listView.BackColor : Color.LightBlue;
+					listViewItem.BackColor = backColor;
+					listView.Items.Add(listViewItem);
+				}
+				else
+				{
+					var addItemToListDelegate = new voidResultListViewListViewItemParams(AddItemToList);
+					Invoke(addItemToListDelegate, listView, listViewItem);
+				}
+			}
+			catch {}
+		}
+
+		private static string GetHostName(string ip)
+		{
+			string result = string.Empty;
+			try
+			{
+				IPHostEntry hostInfo;
+				try
+				{
+					if (Environment.OSVersion.Version.Major > 5) // From Windows Vista
+					{
+						hostInfo = Dns.GetHostEntry(ip);
+					}
+					else
+					{
+						hostInfo = Dns.GetHostByAddress(ip);
+					}
+				}
+				catch
+				{
+					hostInfo = Dns.GetHostEntry(ip);
+				}
+				result = hostInfo.HostName;
+			}
+			catch { }
+			return result;
+		}
+
+		private static bool IsLocalIPAddress(string host)
+		{
+			try
+			{
+				var hostIPs = Dns.GetHostAddresses(host);
+				var localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+
+				foreach (var hostIP in hostIPs)
+				{
+					if (IPAddress.IsLoopback(hostIP))
+					{
+						return true;
+					}
+					foreach(var localIP in localIPs)
+					{
+						if (hostIP.Equals(localIP))
+						{
+							return true;
+						}
+					}
+				}
+			}
+			catch { }
+			return false;
+		}
+
+		private static PhysicalAddress GetMACFromNetworkComputer(IPAddress ipAddress)
+		{
+			PhysicalAddress result = null;
+			try
+			{
+
+				if (ipAddress.AddressFamily == AddressFamily.InterNetworkV6)
+				{
+					var ipv6Address = ipAddress.ToString();
+					var percentIndex = ipv6Address.LastIndexOf("%");
+					if (percentIndex != -1)
+					{
+						ipv6Address = ipv6Address.Substring(0, percentIndex);
+					}
+					var process = Process.Start(new ProcessStartInfo
+					{
+						FileName = "cmd",
+						Arguments = $"/c netsh int ipv6 show neigh | grep {ipv6Address}",
+						UseShellExecute = false,
+						RedirectStandardOutput = true
+					});
+					process.WaitForExit();
+					var output = process.StandardOutput.ReadToEnd();
+					var matches = Regex.Match(output, @"^[a-z0-9:]*\s*(([0-9-]{2}-){5}[0-9-]{2})", RegexOptions.Multiline);
+					if (matches.Success && matches.Groups.Count > 1)
+					{
+						return PhysicalAddress.Parse(matches.Groups[1].Value);
+					}
+					return null;
+				}
+
+				//check what family the ip is from <cref="http://msdn.microsoft.com/en-us/library/system.net.sockets.addressfamily.aspx"/>
+				if (ipAddress.AddressFamily != AddressFamily.InterNetwork)
+				{
+					throw new ArgumentException("The remote system only supports IPv4 addresses");
+				}
+
+				var convertedIp = BitConverter.ToUInt32(ipAddress.GetAddressBytes(), 0);
+				
+				uint srcIp = 0;
+				var macByteArray = new byte[6];
+				int length = macByteArray.Length;
+
+				//call the Win32 API SendARP <cref="http://msdn.microsoft.com/en-us/library/aa366358%28VS.85%29.aspx"/>
+				int arpReply = SendARP(convertedIp, srcIp, macByteArray, ref length);
+
+				if (arpReply != NO_ERROR)
+				{
+					switch (arpReply)
+					{
+						case ERROR_GEN_FAILURE:
+							throw new Exception("A device attached to the system is not functioning. This error is returned on Windows Server 2003 and earlier when an ARP reply to the SendARP request was not received. This error can occur if destination IPv4 address could not be reached because it is not on the same subnet or the destination computer is not operating.");
+						case ERROR_INVALID_PARAMETER:
+							throw new Exception("One of the parameters is invalid. This error is returned on Windows Server 2003 and earlier if either the pMacAddr or PhyAddrLen parameter is a NULL pointer.");
+						case ERROR_BAD_NET_NAME:
+							throw new Exception("The network name cannot be found. This error is returned on Windows Vista and later when an ARP reply to the SendARP request was not received. This error occurs if the destination IPv4 address could not be reached.");
+						case ERROR_BUFFER_OVERFLOW:
+							throw new Exception("The file name is too long. This error is returned on Windows Vista if the ULONG value pointed to by the PhyAddrLen parameter is less than 6, the size required to store a complete physical address.");
+						default:
+							throw new Win32Exception(arpReply);
+					}
+				}
+
+				result = new PhysicalAddress(macByteArray);
+			}
+			catch //(Exception ex)
+			{ }
+			return result;
+		}
+
+		private static string MacAddressToString(PhysicalAddress physicalAddress, char separator = ':')
+		{
+			string mac = physicalAddress.ToString();
+			var friendlyMacAddress = new StringBuilder();
+			for (int i = 0; i < mac.Length; i += 2)
+			{
+				if (i > 0) friendlyMacAddress.Append(separator);
+				friendlyMacAddress.Append(mac[i]);
+				friendlyMacAddress.Append(mac[i + 1]);
+			}
+
+			return friendlyMacAddress.ToString();
+		}
+	}
+}
